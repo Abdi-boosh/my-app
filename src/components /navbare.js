08b18f83920bf19aafe11e625f630e1e879de84d
abdi-boosh <cabdiraxmaanxazan@gmail.com>
2024-12-12 19:21:59 +0300

    first commit

---
+++ src/components /navbare.js
@@ -0,0 +1,16 @@
+import React,{useState} from 'react'
+
+function navbare() {
+  return (
+    <>
+      <nav className='navebar'>
+        <dive className=' navebar-container'>
+            <link to="/"className='navbare-logo'>
+            TRVL</link>
+        </dive>
+      </nav>
+    </>
+  )
+}
+
+export default navbare
